4822b448c8adc06c4e376c3fe894a694ca601ea0
iasmineee <yasminmendesq@gmail.com>
2024-12-16 20:33:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Aguarda 1 segundo para exibir o conteúdo e as barras
-window.addEventListener('load', () => {
-    // Exibe o GIF por 1 segundo
-    setTimeout(() => {
-        // Remove a tela de carregamento (GIF)
-        document.getElementById('loading-screen').style.display = 'none';
-        
-        // Exibe o conteúdo principal
-        document.querySelector('.main-content').style.display = 'block';
-        
-        // Exibe as barras
-        document.body.classList.add('loaded'); // A barra superior e inferior aparecem
-
-        // Mostra o texto na barra superior
-        document.getElementById('top-bar-text').style.display = 'block'; // Mostra o texto
-    }, 1000); // Tempo do gif (1 segundo)
-});
-
 // Espera o tempo de exibição do GIF e então mostra o conteúdo
 window.onload = function() {
     setTimeout(function() {
