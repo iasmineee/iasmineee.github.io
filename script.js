eae404d7cb30fb840dc22d17425f160af82944c0
iasmineee <yasminmendesq@gmail.com>
2024-12-16 21:07:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// Espera o tempo de exibição do GIF e então mostra o conteúdo
-window.onload = function() {
-    setTimeout(function() {
-        document.body.classList.add('show-content'); // Adiciona a classe que mostra o conteúdo
-    }, 1750); // Tempo de 1,75s (1 segundo para o gif + 0,75 segundos)
+// Função para alterar a parte de cima
+function changeTop(image) {
+    document.getElementById('top-img').src = image;
+}
+
+// Função para alterar a parte de baixo
+function changeBottom(image) {
+    document.getElementById('bottom-img').src = image;
+}
+
+// Função para mostrar a área de conteúdo após o gif
+window.onload = function () {
+    setTimeout(function () {
+        document.body.classList.add('show-content');
+    }, 1750);  // 1 segundo de GIF + 0.75 segundos de transição para o fundo
 };
