fc1172114c9ebbac8e4c809fcb92d322444195ed
iasmineee <yasminmendesq@gmail.com>
2024-12-16 20:32:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ window.addEventListener('load', () => {
         document.getElementById('top-bar-text').style.display = 'block'; // Mostra o texto
     }, 1000); // Tempo do gif (1 segundo)
 });
+
+// Espera o tempo de exibição do GIF e então mostra o conteúdo
+window.onload = function() {
+    setTimeout(function() {
+        document.body.classList.add('show-content'); // Adiciona a classe que mostra o conteúdo
+    }, 1750); // Tempo de 1,75s (1 segundo para o gif + 0,75 segundos)
+};
